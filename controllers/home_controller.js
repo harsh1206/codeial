e856c0dc46ddf2723116ce2f464ff68fa9ae6ed8
harsh1206 <harshprakash1206@gmail.com>
2020-08-04 23:04:16 +0530

    add posts feature

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,40 @@
+const Posts=require('../models/posts');
+const Post = require('../models/posts');
+
+
 module.exports.home = function(req, res){
     
+    //   Posts.find({},function(err,posts){
+            
+    //     if(err){
+    //        console.log('error in fetchhing posts')
+    //        return;
+    //     }
+        
+    //     return res.render('home',{
+             
+    //         title:"Home",
+    //         posts:posts
 
-    return res.render('home', {
-        title: "Home"
-    });
+    //     });
+               
+    //   });
+
+    // POpulate the user of each post 
+
+      Post.find({}).populate('user').exec(function(err,posts){
+
+        return res.render('home',{
+             
+            title:"Home",
+            posts:posts
+
+        });         
+         
+
+      })
+    
 }
+
+
 
