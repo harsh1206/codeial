289e3141d36973b6707d6d1d74f08904f6811d79
harsh1206 <harshprakash1206@gmail.com>
2020-08-05 22:35:52 +0530

    add comments amd delete functionality for posts and comments

---
+++ controllers/comments_controller.js
@@ -0,0 +1,51 @@
+const Comment=require('../models/comment');
+const Post=require('../models/posts');
+
+
+module.exports.create=function(req,res){
+     
+   Post.findById(req.body.post,function(err,post){
+
+       if(post){
+ 
+        Comment.create({
+               
+            content:req.body.content,
+            post:req.body.post,
+            user:req.user._id
+                 
+           },function(err,comment){
+              
+              //  handle error 
+             
+               post.comments.push(comment);
+               post.save();
+
+               res.redirect('/');
+           });
+       }
+
+   });
+
+}
+
+module.exports.destroy=function(req,res){
+
+   Comment.findByIdAndDelete(req.params.id,function(err,comment){
+         
+        
+        if(req.user.id==comment.user)
+        {      
+             let postId=comment.post;
+             comment.remove();  
+             Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
+                 return res.redirect('back');
+             });
+        }
+        else{
+            return  res.redirect('back');
+        }
+
+   })
+
+}
